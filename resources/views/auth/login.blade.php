868d56c2ad269c09b82cd7658952f265cd525cc7
Mariana <marianapacor@gmail.com>
2019-10-16 11:45:08 -0300

    centralização do rodapé e formulário do login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,13 +54,13 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary mr-4">
                                     {{ __('Entrar') }}
                                 </button>
 
-                                <div class="col-md-4 offset-md-4">
+                               
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Cadastrar') }}
                                 </button>
