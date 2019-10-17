3a3ffaf2220b7dd6daca5af475047906af5cecc3
Alexandre Monaco <alexandremonaco9@gmail.com>
2019-10-17 11:30:59 -0300

    mais 1 link para home no logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -27,7 +27,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
     <div class="container">
-      <img src="{{url('./admin/assets/images/logo-pig-icon.png')}}" class="light-logo" alt="pigtime" /></span> </a><a class="navbar-brand js-scroll-trigger" href="#page-top">PIG TIME</a>
+      <img src="{{url('./admin/assets/images/logo-pig-icon.png')}}" class="light-logo" alt="pigtime" /></span> </a><a class="navbar-brand js-scroll-trigger" href="/">PIG TIME</a>
       <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
